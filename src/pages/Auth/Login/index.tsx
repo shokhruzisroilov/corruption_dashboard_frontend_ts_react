c5b9fda16b21cdd86f8cfa72c529a5db38412cb3
shokhruzisroilov <shokhruzisroilov@gmail.com>
2025-08-15 15:20:41 +0500

    add vercel config file

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -4,8 +4,8 @@ import { CustomInput } from '@/components/ui/custom-input'
 import { LoginBgImage } from '@/assets/images'
 
 export default function Login() {
-	const [username, setUsername] = useState('Fake admin')
-	const [password, setPassword] = useState('admin')
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
 
 	const handleLogin = (e: React.FormEvent) => {
 		e.preventDefault()
@@ -19,19 +19,19 @@ export default function Login() {
 
 	return (
 		<div
-			className='w-full min-h-screen flex items-center justify-center'
+			className='w-full min-h-screen flex items-center justify-center bg-cover bg-center px-4 sm:px-6 lg:px-8'
 			style={{ backgroundImage: `url(${LoginBgImage})` }}
 		>
 			<form
 				onSubmit={handleLogin}
-				className='w-full max-w-sm bg-white rounded-[28px] shadow-[0px_0px_1px_0px_#1C293D14] px-6 py-8 space-y-5'
+				className='w-full max-w-sm sm:max-w-md bg-white rounded-[20px] sm:rounded-[28px] shadow-[0px_0px_1px_0px_#1C293D14] px-5 sm:px-6 md:px-8 py-6 sm:py-8 space-y-5'
 			>
 				{/* Title */}
 				<div className='text-center space-y-1'>
-					<h2 className='font-bold text-[32px] leading-[120%] tracking-[0] text-[#282F3D]'>
+					<h2 className='font-bold text-[24px] sm:text-[28px] md:text-[32px] leading-[120%] tracking-[0] text-[#282F3D]'>
 						Tizimga kirish
 					</h2>
-					<p className='font-medium text-[16px] leading-[150%] tracking-[0] text-center align-middle text-[#0F132499]'>
+					<p className='font-medium text-[14px] sm:text-[15px] md:text-[16px] leading-[150%] tracking-[0] text-center align-middle text-[#0F132499]'>
 						Login va parolingizni kiriting
 					</p>
 				</div>
@@ -45,9 +45,6 @@ export default function Login() {
 						value={username}
 						onChange={e => setUsername(e.target.value)}
 					/>
-					{/* <p className='text-xs text-gray-400'>
-						This is a hint text to help user.
-					</p> */}
 				</div>
 
 				{/* Password */}
@@ -60,15 +57,12 @@ export default function Login() {
 						onChange={e => setPassword(e.target.value)}
 						showPasswordToggle
 					/>
-					{/* <p className='text-xs text-gray-400'>
-						This is a hint text to help user.
-					</p> */}
 				</div>
 
 				{/* Submit */}
 				<Button
 					type='submit'
-					className='w-full bg-[#12B76A] hover:bg-[#4ac48b] cursor-pointer'
+					className='w-full bg-[#12B76A] hover:bg-[#4ac48b] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed'
 					disabled={isDisabled}
 				>
 					Kirish
